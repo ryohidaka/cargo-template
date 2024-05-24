42a6278100de3888bb8d0d4490dcf55bdb55985e
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-05-25 00:28:06 +0900

    feat: Add test.

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,15 @@
 pub fn run() {
     println!("Hello from the library!");
 }
+
+// Test code
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run() {
+        run();
+        assert!(true);
+    }
+}
